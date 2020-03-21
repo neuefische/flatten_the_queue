c65790b5448152623c143d04f2c2281b6a89e5e0
Ludmila Antuofermo <l.antuofermo@web.de>
2020-03-21 23:20:28 +0100

    Refactoring style components

--- client/src/components/Range/Handles.js
+++ client/src/components/Range/Handles.js
@@ -1,31 +1,47 @@
 import React from 'react'
+import styled from 'styled-components/macro'
 
 export default function Handle({
   handle: { id, value, percent },
   getHandleProps,
 }) {
   return (
-    <div
+    <Handler
       style={{
         left: `${percent}%`,
-        position: 'absolute',
-        marginLeft: -15,
-        marginTop: 28,
-        zIndex: 2,
-        width: 25,
-        height: 25,
-        border: 0,
-        textAlign: 'center',
-        cursor: 'pointer',
-        borderRadius: '50%',
-        backgroundColor: '#EE833F',
-        color: '#EE833F',
       }}
       {...getHandleProps(id)}
     >
-      <div style={{ fontFamily: 'Roboto', fontSize: 11, marginTop: -35 }}>
-        {value}
-      </div>
-    </div>
+      <RangeLabel>
+        {value === 1
+          ? 'leer'
+          : value === 2
+          ? 'fast leer'
+          : value === 3
+          ? 'fast voll'
+          : 'voll'}
+      </RangeLabel>
+    </Handler>
   )
 }
+
+const RangeLabel = styled.div`
+  font-size: 12px;
+  margin-top: -20px;
+  margin-left: -25px;
+  width: 70px;
+`
+const Handler = styled.div`
+  position: absolute;
+  margin-left: -15px;
+  margin-top: 30px;
+  z-index: 2;
+  width: 20px;
+  height: 20px;
+  border: 0;
+  text-align: center;
+  cursor: pointer;
+  border-radius: 50%;
+  background-color: #ee833f;
+  color: #ee833f;
+`
