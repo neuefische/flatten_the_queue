a2d21fdf3243e46f7a1fd256acc53fc204b18452
Julie Mahlke <mail@julie-alexandria.de>
2020-03-21 19:30:34 +0100

    Add font and global styling

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,19 @@ import styled from 'styled-components/macro'
 import Navigation from './common/Navigation'
 import AboutPage from './pages/AboutPage'
 import HomePage from './pages/HomePage'
-
+import ResultPage from './pages/ResultPage'
 
 export default function App() {
   return (
     <Router>
       <AppGrid>
+        <Header>flatten the queue</Header>
         <Switch>
           <Route exact path="/">
             <HomePage />
+          </Route>
+          <Route path="/result">
+            <ResultPage />
           </Route>
           <Route path="/about">
             <AboutPage />
@@ -26,11 +30,23 @@ export default function App() {
 
 const AppGrid = styled.div`
   display: grid;
-  grid-template-rows: auto 48px;
+  grid-template-rows: 48px auto 48px;
   position: fixed;
   left: 0;
   right: 0;
   top: 0;
   bottom: 0;
   height: 100%;
+`
+
+const Header = styled.header`
+  text-align: center;
+  background-color: #f77d33;
+  color: #fff;
+  font-weight: 900;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  letter-spacing: 1px;
 `
---
+++ client/src/pages/ResultPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import List from '../components/List'
+
+export default function ResultPage() {
+  return (
+    <main>
+      <h1>Supermärkte in Deiner Nähe</h1>
+    </main>
+  )
+}
