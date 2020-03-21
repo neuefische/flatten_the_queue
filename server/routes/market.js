06194a500b65213cec545a1075d869ba1d30392b
Jan Robert <janmartinrobert@gmail.com>
2020-03-21 23:11:39 +0100

    Add query parameters
    Co-authored-by: Jan Grade <40357932+jangrade@users.noreply.github.com>
    Co-authored-by: hagenbrandt <hagen-brandt@web.de>

--- server/routes/market.js
+++ server/routes/market.js
@@ -1,27 +1,60 @@
 import { Router } from 'express'
 import MOCK_DATA from '../mockdata/testdata.json'
-const router = Router()
-const https = require('https')
 import axios from 'axios'
 
+const { GAPIKEY } = process.env
+
+const router = Router()
+const https = require('https')
+
 router.get('/nearby', async (req, res, next) => {
-  const { lat, lng } = await getCoordinates(req.query.address)
-  const places = await placeSearch(lat, lng)
-  console.log(places)
+  const { input, radius } = req.query
+  const markets = await getPlaces(input, radius)
+  res.json(markets)
 })
 
 export default router
 
-async function getCoordinates(query) {
-  const coordinates = await axios.get(
-    `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${query}&inputtype=textquery&fields=geometry&key=AIzaSyB-ksg4SiiLPpU4Xm7V5yt34E1C8SKdiLk`
-  )
-  return coordinates.data.candidates[0].geometry.location
+function getPlaces(input, radius) {
+  return getCoordinates(input)
+    .then(({ lat, lng }) => placeSearch(lat, lng, radius))
+    .then(places =>
+      places.map(place => {
+        const [street, city] = place.vicinity.split(',')
+        return {
+          name: place.name,
+          id: place.id,
+          street: street.trim(),
+          city: city.trim(),
+        }
+      })
+    )
+    .catch(err => {
+      console.log(err)
+      return []
+    })
 }
 
-async function placeSearch(latitude, longitude, radius = 5000) {
-  const places = await axios.get(
-    `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=${radius}&types=supermarket&key=AIzaSyD7MB-Afk1X3Buu0brPpYO3SK_2DILXtYQ`
-  )
-  return places.data.results
+function getCoordinates(input) {
+  return axios
+    .get(
+      `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${input}&inputtype=textquery&fields=geometry&key=${GAPIKEY}`
+    )
+    .then(coordinates => {
+      return coordinates.data.status === 'OK'
+        ? coordinates.data.candidates[0].geometry.location
+        : { lat: 1, lng: 1 }
+    })
+    .catch(err => {
+      console.log(err)
+      return { lat: 1, lng: 1 }
+    })
+}
+
+function placeSearch(latitude, longitude, radius = 1000) {
+  return axios
+    .get(
+      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=${radius}&types=grocery_or_supermarket&key=${GAPIKEY}`
+    )
+    .then(places => places.data.results)
 }
